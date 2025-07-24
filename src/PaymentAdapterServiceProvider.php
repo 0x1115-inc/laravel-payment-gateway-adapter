881877159e77734770849c59ec5c3a86dc5f2b60
Duog Lee <duoglee@0x1115.com>
2025-07-24 19:04:24 +0700

    add PaymentAdapterServiceProvider and configuration file for payment adapters

---
+++ src/PaymentAdapterServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright 2025 0x1115 Inc <info@0x1115.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace MCXV\LaravelPaymentAdapter;
+
+use Illuminate\Support\ServiceProvider;
+class PaymentAdapterServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/payment_adapter.php' => config_path('payment_adapter.php')
+        ]);
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/payment_adapter.php',
+            'payment_adapter'
+        );
+    }
+}
---
+++ src/config/payment_adapter.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright 2025 0x1115 Inc <info@0x1115.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+return [
+    /*
+    |----------------------------------------------------------------------
+    | Default Payment Adapter
+    |----------------------------------------------------------------------
+    |
+    | This option controls the default payment adapter that will be used
+    | by the application. 
+    |    
+    */
+    
+    'default' => env('PAYMENT_ADAPTER_DEFAULT', 'cpg'),
+
+    /*
+    |----------------------------------------------------------------------
+    | Payment Adapter Drivers
+    |----------------------------------------------------------------------
+    |
+    | Here you may configure the payment adapter drivers that will be used
+    | by the application. Each driver should have its own configuration
+    | options, such as API keys, secrets, and other necessary credentials.
+    |
+    | Drivers: "cpg", "coinbase"
+    |
+    */
+
+    'drivers' => [
+        'cpg' => [
+            'apikey' => env('CPG_API_KEY', '')            
+        ],
+        'coinbase' => [
+            'api_key' => env('COINBASE_API_KEY', ''),
+            'api_secret' => env('COINBASE_API_SECRET', ''),
+            'webhook_secret' => env('COINBASE_WEBHOOK_SECRET', ''),
+        ]
+    ],
+],
